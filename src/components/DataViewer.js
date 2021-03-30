ed2afd14dab19afe8fcfb9c9ecafd0baff87aecf
윤재상 <banana2692@daum.net>
2021-03-30 23:56:25 +0900

    Feat. Edit tag on DataViewer

--- src/components/DataViewer.js
+++ src/components/DataViewer.js
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import { IfFirebaseAuthed } from '@react-firebase/auth';
 import { FirebaseDatabaseNode } from '@react-firebase/database';
 
 import { Button, ButtonGroup } from 'react-bootstrap';
@@ -29,7 +29,7 @@ const ReceiptButton = (props) => {
   );
 };
 
-const TransactionOnTable = ({ data }) => {
+const TransactionOnTable = ({ data, updateTran, targetYM }) => {
   const number_format = new Intl.NumberFormat('ko-KR', {
     signDisplay: 'never',
     style: 'decimal',
@@ -88,10 +88,27 @@ const TransactionOnTable = ({ data }) => {
               [Cell.balance, number_format.format(tran.balance)],
               [Cell.delta, format_curr(tran.delta)],
               [Cell.note, tran.note],
-              [Cell.tag, tran.tag || '-'],
+              [
+                Cell.tag,
+                <Button
+                  children={tran.tag || '추가'}
+                  className='p-0 m-0'
+                  size='sm'
+                  variant={
+                    tran.tag === undefined ? 'outline-danger' : 'secondary'
+                  }
+                  onClick={() => {
+                    const result = prompt('태그 바꾸기', tran.tag);
+                    if (result !== null) {
+                      updateTran(Object.assign(tran, { tag: result }));
+                    }
+                  }}
+                />,
+              ],
               [ReceiptButton, null],
-            ].map(([Com, content]) => (
+            ].map(([Com, content], td_idx) => (
               <td
+                key={td_idx}
                 children={<Com children={content} pk={tran.timestamp} />}
                 className='border'
               />
@@ -105,47 +122,63 @@ const TransactionOnTable = ({ data }) => {
 
 class DataViewer extends Component {
   state = {
-    targetYM: '2021-02',
+    targetYM: moment().format('YYYY-MM'),
     tranOnMonth: {},
   };
-
   YmButtonOnClick(ym, e) {
     this.setState({ targetYM: ym });
+  }
+
+  updateTran(new_tran) {
+    if (new_tran.YYYY_MM !== this.state.targetYM) throw new_tran;
+    const query = {
+      path: `transactions/${new_tran.YYYY_MM}/${new_tran.timestamp}`,
+      value: new_tran,
+      type: 'update',
+    };
+    const ref = firebase.app().database().ref(query.path);
+    ref.update(query.value);
   }
 
   render() {
     return (
       <div>
-        <IfFirebaseAuthed>
-          <FirebaseDatabaseNode
-            path='month_of_transactions/'
-            children={(d) => (
-              <ButtonGroup toggle className='my-3'>
-                <Button size='sm' children='조회연월' variant='outline-dark' />
-                {d.value &&
-                  Object.entries(d.value).map(([ym]) => (
-                    <Button
-                      size='sm'
-                      variant='outline-dark'
-                      children={ym}
-                      onClick={this.YmButtonOnClick.bind(this, ym)}
-                    />
-                  ))}
-              </ButtonGroup>
-            )}
-          />
-          <br />
-          {this.state.targetYM === null ? (
-            <>Please select taget date</>
-          ) : (
-            <div className='w-100 d-flex flex-column align-items-center'>
-              <FirebaseDatabaseNode
-                path={`transactions/${this.state.targetYM}`}
-                children={(d) => <TransactionOnTable data={d} />}
-              />
-            </div>
+        <FirebaseDatabaseNode
+          path='month_of_transactions/'
+          children={(d) => (
+            <ButtonGroup toggle className='my-3'>
+              <Button size='sm' children='조회연월' variant='outline-dark' />
+              {d.value &&
+                Object.entries(d.value).map(([ym]) => (
+                  <Button
+                    key={ym}
+                    size='sm'
+                    variant='outline-dark'
+                    children={ym}
+                    onClick={this.YmButtonOnClick.bind(this, ym)}
+                  />
+                ))}
+            </ButtonGroup>
           )}
-        </IfFirebaseAuthed>
+        />
+        <br />
+        {this.state.targetYM === null ? (
+          <>Please select taget date</>
+        ) : (
+          <div className='w-100 d-flex flex-column align-items-center'>
+            <FirebaseDatabaseNode
+              path={`transactions/${this.state.targetYM}`}
+              orderByKey={true}
+              children={(d) => (
+                <TransactionOnTable
+                  data={d}
+                  updateTran={(d) => this.updateTran(d)}
+                  targetYM={this.state.targetYM}
+                />
+              )}
+            />
+          </div>
+        )}
       </div>
     );
   }
